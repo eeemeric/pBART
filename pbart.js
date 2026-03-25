b4ce0ef22a2964c09d26d7c81ea297429c096756
erik e emeric <erik.emeric@gmail.com>
2026-03-25 13:47:44 -0400

    Log game state in game loop
    Added logging to game loop to track state.

--- pbart.js
+++ pbart.js
@@ -433,9 +433,10 @@ class pBART {
 	}
 
 	gameLoop() {
-		this.update();
-		this.draw();
-		requestAnimationFrame(() => this.gameLoop());
+	    console.log('Loop tick, state:', this.game_state);
+	    this.update();
+	    this.draw();
+	    requestAnimationFrame(() => this.gameLoop());
 	}
 }
 
